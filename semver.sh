1e1d9d5bfcf34ef08093dfed53ae9f97f64e9193
grzegorzgniadek <93030912+grzegorzgniadek@users.noreply.github.com>
2024-09-11 15:01:36 +0200

    Update semver.sh

--- semver.sh
+++ semver.sh
@@ -24,26 +24,28 @@ while [[ $# -gt 0 ]]; do
 done
 
 COMMIT=$(git show -s --format=%B)
-COMMIT=${COMMIT,,}
+COMMIT=$(echo "$COMMIT" | tr '[:upper:]' '[:lower:]')
 
 MAJOR_MATCH="(major|breaking)[:|\/|\(]"
 MINOR_MATCH="feat[:|\/|\(]"
 PATCH_MATCH="fix[:|\/|\(]"
-[[ $OLD_VERSION == "no" ]] && OLD_VERSION=$(git describe --tags --abbrev=0)
-[[ $OLD_VERSION =~ "-(.*)$" ]] && HOTFIX=true
 
-OLD_VERSION=$(sed 's/.*\([0-9]\+\.[0-9]\+\.[0-9]\+\)$/\1/' <<<"$OLD_VERSION")
+[[ $OLD_VERSION == "no" ]] && OLD_VERSION=$(git tag -l 'v*.*.*'| sort -V | tail -n 1)
+
+OLD_VERSION=$(sed 's:^v::' <<<"$OLD_VERSION")
 if [[ $HOTFIX == "true" ]]; then
-  BASE=$(sed 's/^.*\([0-9]\+\.[0-9]\+\.[0-9]\+\)-.*/\1/' <<<"$OLD_VERSION")
+  BASE=$(sed 's:-.*::g' <<<"$OLD_VERSION")
   [[ $OLD_VERSION =~ "-" ]] &&
-    HOTFIX=$(sed 's:^.*-::g' <<<"$OLD_VERSION") || HOTFIX=0
+    HOTFIX=$(sed 's:^.*-::g' <<<"$OLD_VERSION") ||
+    HOTFIX=0
 
   HOTFIX=$((HOTFIX + 1))
   VERSION="v$BASE-$HOTFIX"
+
 else
-  MAJOR=$(sed 's/^\([0-9]\+\)\.[0-9]\+\.[0-9]\+.*$/\1/' <<<"$OLD_VERSION")
-  MINOR=$(sed 's/^[0-9]\+\.\([0-9]\+\)\.[0-9]\+.*$/\1/' <<<"$OLD_VERSION")
-  PATCH=$(sed 's/^[0-9]\+\.[0-9]\+\.\([0-9]\+\).*/\1/' <<<"$OLD_VERSION")
+  MAJOR=$(sed 's:\..*$::' <<<"$OLD_VERSION")
+  MINOR=$(sed 's:^[0-9]*\.\([0-9]*\)\..*$:\1:' <<<"$OLD_VERSION")
+  PATCH=$(sed 's:^[0-9]*\.[0-9]*\.\([0-9]*\).*$:\1:' <<<"$OLD_VERSION")
 
   if [[ $COMMIT =~ $MAJOR_MATCH ]]; then
     MAJOR=$((MAJOR + 1))
